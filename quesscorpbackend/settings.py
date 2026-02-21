7a50ba53284395fda872bc2de2fdacf678e8a04e
abdulafjal786 <abdulansari251298@gmail.com>
2026-02-21 12:52:35 +0530

    update

--- quesscorpbackend/settings.py
+++ quesscorpbackend/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'quesscorpbackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'out')],  # Next build folder
+        'DIRS': [os.path.join(BASE_DIR, "out")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- quesscorpbackend/urls.py
+++ quesscorpbackend/urls.py
@@ -1,22 +1,5 @@
-"""
-URL configuration for quesscorpbackend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include,re_path
-
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.views.static import serve
 from django.conf import settings
@@ -26,17 +9,17 @@ BASE_OUT = os.path.join(settings.BASE_DIR, "out")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # API routes
     path('api/accounts/', include('accounts.urls')),
-    path('employee/', include('employee.urls')),
-    
-    
-       # Serve Next.js _next static assets
-    re_path(r'^_next/(?P<path>.*)$', serve, {'document_root': os.path.join(BASE_OUT, '_next')}),
+    path('api/employee/', include('employee.urls')),
 
-    # Specific static pages
-    path('employee/', TemplateView.as_view(template_name=os.path.join(BASE_OUT, 'employee.html'))),
-    path('attendance/', TemplateView.as_view(template_name=os.path.join(BASE_OUT, 'attendance.html'))),
+    # Serve Next.js _next static files
+    re_path(r'^_next/(?P<path>.*)$',
+            serve,
+            {'document_root': os.path.join(BASE_OUT, '_next')}),
 
-    # Catch-all -> index.html
-    re_path(r'^.*$', TemplateView.as_view(template_name=os.path.join(BASE_OUT, 'index.html'))),
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('employee/', TemplateView.as_view(template_name='employee.html')),
+    path('attendance', TemplateView.as_view(template_name='attendance.html')),
 ]
